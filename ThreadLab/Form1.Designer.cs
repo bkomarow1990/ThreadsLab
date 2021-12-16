d597b688fadb02d6c338f62a0f938e100148c82a
Gnid_ct84 <Gnid_ct84@student.itstep.org>
2021-12-16 21:49:47 +0200

    Add project files.

---
+++ ThreadLab/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace ThreadLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.countNumeric = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.pauseBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.threadNumberNumeric = new System.Windows.Forms.NumericUpDown();
+            this.numsListBox = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.countNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threadNumberNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // countNumeric
+            // 
+            this.countNumeric.Location = new System.Drawing.Point(124, 17);
+            this.countNumeric.Name = "countNumeric";
+            this.countNumeric.Size = new System.Drawing.Size(162, 22);
+            this.countNumeric.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Count Threads:";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(293, 15);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(152, 24);
+            this.startBtn.TabIndex = 2;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // pauseBtn
+            // 
+            this.pauseBtn.Location = new System.Drawing.Point(376, 43);
+            this.pauseBtn.Name = "pauseBtn";
+            this.pauseBtn.Size = new System.Drawing.Size(152, 24);
+            this.pauseBtn.TabIndex = 5;
+            this.pauseBtn.Text = "Pause";
+            this.pauseBtn.UseVisualStyleBackColor = true;
+            this.pauseBtn.Click += new System.EventHandler(this.pauseBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Thread to Pause/Resume:";
+            // 
+            // threadNumberNumeric
+            // 
+            this.threadNumberNumeric.Location = new System.Drawing.Point(208, 45);
+            this.threadNumberNumeric.Name = "threadNumberNumeric";
+            this.threadNumberNumeric.Size = new System.Drawing.Size(162, 22);
+            this.threadNumberNumeric.TabIndex = 3;
+            // 
+            // numsListBox
+            // 
+            this.numsListBox.FormattingEnabled = true;
+            this.numsListBox.ItemHeight = 16;
+            this.numsListBox.Location = new System.Drawing.Point(15, 73);
+            this.numsListBox.Name = "numsListBox";
+            this.numsListBox.Size = new System.Drawing.Size(773, 356);
+            this.numsListBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numsListBox);
+            this.Controls.Add(this.pauseBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.threadNumberNumeric);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.countNumeric);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.countNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threadNumberNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown countNumeric;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button pauseBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown threadNumberNumeric;
+        private System.Windows.Forms.ListBox numsListBox;
+    }
+}
+
---
+++ ThreadLab/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadLab
+{
+    public partial class Form1 : Form
+    {
+        Thread[] threads = null;
+        EventWaitHandle[] waitHandlers = null;
+        bool[] states = null;
+        private bool isStart = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (countNumeric.Value <= 0)
+            {
+                MessageBox.Show("Count of threads must be more than 0");
+                return;
+            }
+            if (isStart)
+            {
+                numsListBox.Items.Clear();
+            }
+            ParameterizedThreadStart threadstart = new ParameterizedThreadStart(NumsPrint);
+            threads = new Thread[(int)countNumeric.Value];
+            waitHandlers = new ManualResetEvent[(int)countNumeric.Value];
+            states = new bool[(int)countNumeric.Value];
+            for (int i = 0; i < countNumeric.Value; i++)
+            {
+                threads[i] = new Thread(threadstart);
+                threads[i].IsBackground = true;
+                waitHandlers[i] = new ManualResetEvent(initialState: true);
+                states[i] = false;
+            }
+            for (int i = 0; i < countNumeric.Value; i++)
+            {
+                threads[i].Start((object)(i+1));
+            }
+            isStart = !isStart;
+        }
+        private void NumsPrint(object obj)
+        {
+            int thread_index = (obj as int?).GetValueOrDefault()-1;
+            int i = thread_index * 100;
+            while (true)
+            {
+            
+                waitHandlers[thread_index].WaitOne();
+                lock (this) { 
+                    this.Invoke(new Action(() =>
+                    {
+                        numsListBox.Items.Add(++i);
+                    }));
+                }
+            }
+        }
+
+        private void pauseBtn_Click(object sender, EventArgs e)
+        {
+            
+            int index = (int)threadNumberNumeric.Value;
+            if (index < 0 || index>=states.Length)
+            {
+                MessageBox.Show("Incorrect index of thread number which you want to pause/resume");
+                return;
+            }
+            states[index] = !states[index];
+            if (states[index])
+            {
+                waitHandlers[index].Reset();
+                return;
+            }
+            waitHandlers[index].Set();
+        }
+    }
+}
